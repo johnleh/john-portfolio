b2d7342d8f8102e35df1bed77d33aa484d8ce425
johnleh <johnleh@live.unc.edu>
2023-02-27 16:28:55 -0500

    under construction page

---
+++ comps/UnderConstruction.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function UnderConstruction() {
+  return (
+    <div className="vw-100 vh-100 align-middle text-center border">
+        <div className="position-absolute top-50 start-50 translate-middle">
+            <h1>Website Under Construction!</h1>
+            <h2>Please Come Back Later.</h2>
+        </div>
+    </div>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
 import DefaultLayout from '@layouts/DefaultLayout';
 import 'bootstrap/dist/css/bootstrap.css'
-
+import UnderConstruction from '@comps/UnderConstruction'
 function MyApp({ Component, pageProps }) {
-
+  if(process.env.NODE_ENV == "production") {
+    return (
+      <UnderConstruction/>
+    )
+  }
   if(Component.getLayout) {
     return Component.getLayout(<Component {...pageProps} />)
   } else {
