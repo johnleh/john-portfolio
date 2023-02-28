ee2583e011354a0fedc4f5d2e54a3876de952fbd
johnleh <johnleh@live.unc.edu>
2023-02-27 22:47:17 -0500

    changed loading screen

--- layouts/LoadingContent.js
+++ layouts/LoadingContent.js
@@ -4,6 +4,10 @@ import React from 'react'
 
 export default function LoadingScreen() {
   return (
-    <h1>Loading</h1>
+    <div className="vw-100 vh-100 align-middle text-center">
+        <div className="position-absolute top-50 start-50 translate-middle">
+            <h1>Loading</h1>
+        </div>
+    </div>
   )
 }
