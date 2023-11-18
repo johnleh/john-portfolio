2264d99b263a9d051f1a421119668d5b5903258c
johnleh <johnleh@live.unc.edu>
2023-11-17 19:12:18 -0500

    removed routing on error

--- src/app/projects/[projectId]/page.js
+++ src/app/projects/[projectId]/page.js
@@ -1,20 +1,17 @@
-"use client";
 import React from 'react';
 import PageContent from '@comps/PageContent'
 import ProjectData from 'src/app/projects/ProjectData'
-import { useRouter } from 'next/navigation'
 
 
 export default function Projects({ params }) {
-  const router = useRouter()
   let pageData
   try {
     const projectId = params.projectId
     pageData = ProjectData[projectId]
     pageData.projectId = projectId
   } catch {
-    router.push('/projects')
-    return <></>
+    pageData = ProjectData[0]
+    pageData.projectId = 0
   }
 
   return (
