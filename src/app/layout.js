1e310f62c2aaa74f681fa838a3173190f4435637
johnleh <john.lehouillier@gmail.com>
2026-05-28 16:26:47 -0400

    Moved SpeedInsights

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,8 +23,9 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <SpeedInsights />
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <SpeedInsights />
+        {children}</body>
     </html>
   );
 }
